f2769a535effc109b061459347682ffb9c9a4b21
Mario Lazzari <mario.lazzari@gmail.com>
2019-12-10 20:51:14 +0100

    Done lesson 3

--- myproject/App.js
+++ myproject/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const { container, header, boldText, body } = styles;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={container}>
+      <View style={header}>
+        <Text style={boldText}>Ciao!</Text>
+      </View>
+      <View style={body}>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Necessitatibus blanditiis libero, consequatur beatae neque, ex a nihil
+          atque cum explicabo veniam rem, aliquid quasi. Accusantium iste
+          excepturi in dolorum eligendi!
+        </Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20
+  }
 });
+
+export default App;
